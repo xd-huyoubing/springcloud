42d252c141e5e0f23a748eef9e18e1e7fa26c539
xd-huyoubing <18291424496@163.com>
2018-06-05 17:10:11 +0800

    提交

---
+++ microservicecloud-consumer-dept-80/src/main/java/edu/xidian/springcloud/DeptConsumer80_APP.java
@@ -0,0 +1,14 @@
+package edu.xidian.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptConsumer80_APP {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeptConsumer80_APP.class, args);
+
+	}
+
+}
